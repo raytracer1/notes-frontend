25a0943184afbdff8032b42b44b8bd02c031a6eb
raytracer1 <2996385216@qq.com>
2023-02-13 12:24:13 +0800

    implement header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
+import { Nav, Navbar, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import './style.scss';
 
 function Header() {
     return (
       <div className='header'>
-        header
+        <div className='container'>
+          <a className='logo' href='/' target="_top">
+            <img className='logo' src='logo192.png' alt='logo' />
+          </a>
+          <Navbar>
+            <Nav navbar>
+              <div className="nav">
+                <NavItem>
+                  <NavLink className="nav-link"  to='/home'>Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/space'>Create Learning Space</NavLink>
+                </NavItem>
+              </div>
+            </Nav>
+          </Navbar>
+        </div>
       </div>
     );
   }

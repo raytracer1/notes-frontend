02fc9780b3cf942902255c8b7b0937acbfa58e29
raytracer1 <2996385216@qq.com>
2023-02-21 00:02:02 +0800

    import bootstrap

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,47 @@
-import {  Button } from 'reactstrap';
+import { useState } from "react";
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem,
+  Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  }
 
   return (
     <div className='header'>
-      <div className='container'>
-        <a className='logo' href='/' target="_top">
-          <img className='logo' src='logo192.png' alt='logo' />
-        </a>
-        <div className="nav">
-          <NavLink className="nav-link" to='/home'>
-            <i className="fa fa-home fa-lg"></i>home
-          </NavLink>
-          <NavLink className="nav-link" to='/space'>
-            <i className="fa fa-info fa-lg"></i>Create Learning Space
-          </NavLink>
+      <Navbar dark expand="md">
+        <div className="container">
+          <NavbarToggler onClick={toggleNav} />
+          <NavbarBrand href="/">
+            <img className="img" src='logo192.png' alt='log' />
+          </NavbarBrand>
+          <Collapse isOpen={isNavOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink className="nav-link" to='/'>
+                  <span className="fa fa-home fa-lg"></span>Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to='/space'>
+                  <span className="fa fa-info fa-lg"></span>Create Learning Space
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className="button" navbar>
+              <NavItem>
+                <Button outline>
+                  <span className="fa fa-sign-in fa-lg"></span>Login
+                </Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </div>
-        <Button outline className='button'>
-          <i className="fa fa-sign-in fa-lg"></i> Login
-        </Button>
-      </div>
+      </Navbar>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';

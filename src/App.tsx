c7a31682a7586eb4047e767f52f59c29538bb479
raytracer1 <2996385216@qq.com>
2023-02-21 17:03:14 +0800

    add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Route, Routes, Navigate } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Space from './components/Space';
+
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/space" element={<Space />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-      <Footer />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Header from '../Header';
+import Footer from '../Footer';
+import Home from '../Home';
+import Space from '../Space';
+import './style.scss';
+
+function Main() {
+    return (
+      <div className="main">
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/space" element={<Space />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+  
+export default Main;

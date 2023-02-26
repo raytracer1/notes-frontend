a99f98a0ba71cc908f228aad67c7f06092999519
raytracer1 <2996385216@qq.com>
2023-02-26 17:19:33 +0800

    fix footer mailto href

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -36,7 +36,7 @@ function Footer() {
                 <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
                 <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
                 <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                <a className="btn btn-social-icon" href="xxxx@gmail.com"><i className="fa fa-envelope-o"></i></a>
+                <a className="btn btn-social-icon" href="mailto:xxxx@gmail.com"><i className="fa fa-envelope-o"></i></a>
               </div>
             </div>
           </div>

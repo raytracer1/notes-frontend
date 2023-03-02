ac8788db2d4c9aa67192310edfc8d0c085ea946b
raytracer1 <2996385216@qq.com>
2023-03-02 11:21:51 +0800

    change users to user

--- src/service/authService.tsx
+++ src/service/authService.tsx
@@ -2,16 +2,16 @@ import { apiInstance as Axios } from '../api/requestInterceptor';
 
 //Login
 const loginUser = (email: string, passWord: string) => {
-  return Axios.post('users/login', { email: email, password: passWord } );
+  return Axios.post('user/login', { email: email, password: passWord } );
 }
 
 //Logout
 const logoutUser = () => {
-  return Axios.get('users/logout');
+  return Axios.get('user/logout');
 }
 
 const signupUser = (userName: string, email: string, passWord: string) => {
-  return Axios.post('users/signup', { userName: userName, email: email, password: passWord });
+  return Axios.post('user/signup', { userName: userName, email: email, password: passWord });
 }
 
 export {

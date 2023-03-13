a439346ed23a2cfee6c747c406d0e2783c271f7d
raytracer1 <2996385216@qq.com>
2023-03-13 11:28:55 +0800

    support home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,26 @@
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import './style.scss';
 
 function Home() {
-    return (
-      <div className='home'>
+  const authStatus = useAppSelector((state) => state.auth.authenticated);
+
+  return (
+    <div className='home'>
+      <div className='header'>
+        <div className='description'>
+          <h3>
+            {
+              authStatus ? "Join a learning space below" :
+                "Sign up to start learning"
+            }
+          </h3>
+        </div>
+      </div>
+      <div className='body'>
         home
       </div>
-    );
-  }
+    </div>
+  );
+}
   
 export default Home;

b409ec4e01e941c6a0b0630b47a6284892cf5b7c
raytracer1 <2996385216@qq.com>
2023-04-04 20:52:45 +0800

    modify title

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,6 @@
 import { Gender, Country } from '../interface';
 
-export const baseURL = 'https://phynotes:3444';
+export const baseURL = 'https://3.83.251.117:3444';
 
 export const GENDER_OPTIONS : Gender[] = [
     { value : 'secret' },
